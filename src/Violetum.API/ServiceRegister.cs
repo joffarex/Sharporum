08aeb82f5c285f23dbf46475eff50d754e909101
Tornike Goshadze <joffarex@gmail.com>
2020-05-07 15:45:00 +0000

    Setup Service injector

---
+++ src/Violetum.API/ServiceRegister.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Violetum.Application;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceRegister
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection @this)
+        {
+            Type serviceType = typeof(Service);
+            IEnumerable<TypeInfo> definedType = serviceType.Assembly.DefinedTypes;
+
+            IEnumerable<TypeInfo> services = definedType
+                .Where(x => x.GetTypeInfo().GetCustomAttribute<Service>() != null);
+
+            foreach (TypeInfo service in services)
+            {
+                @this.AddTransient(service);
+            }
+
+            // @this.AddTransient<IManager, Manager>();
+
+            return @this;
+        }
+    }
+}
