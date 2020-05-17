5643ed1ba1482b477695d5b2701b7ad04535fdda
Tornike Goshadze <joffarex@gmail.com>
2020-05-17 15:55:27 +0000

    Update Post Service Interface

--- src/Violetum.ApplicationCore/Interfaces/IPostService.cs
+++ src/Violetum.ApplicationCore/Interfaces/IPostService.cs
@@ -1,9 +1,17 @@
-﻿using Violetum.ApplicationCore.ViewModels;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Violetum.ApplicationCore.Dtos.Post;
+using Violetum.ApplicationCore.ViewModels;
+using Violetum.Domain.Entities;
 
 namespace Violetum.ApplicationCore.Interfaces
 {
     public interface IPostService
     {
         PostViewModel GetPost(string postId);
+        Task<IEnumerable<PostViewModel>> GetPosts(SearchParams searchParams, Paginator paginator);
+        Task<PostViewModel> CreatePost(PostDto postDto);
+        Task<PostViewModel> UpdatePost(string postId, string userId, UpdatePostDto updatePostDto);
+        Task<bool> DeleteProduct(string postId, string userId, DeletePostDto deletePostDto);
     }
 }
