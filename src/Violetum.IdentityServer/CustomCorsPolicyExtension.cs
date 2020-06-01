ba1f63039310de5ea86076edaf71d93d07898ad2
Tornike Goshadze <joffarex@gmail.com>
2020-06-01 19:12:52 +0400

    Add Angular SPA with simple authentication

---
+++ src/Violetum.IdentityServer/CustomCorsPolicyExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Services;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class CustomCorsPolicyExtension
+    {
+        public static IIdentityServerBuilder AddCustomCorsPolicy(this IIdentityServerBuilder builder)
+        {
+            var existingCors = builder.Services.Where(x => x.ServiceType == typeof(ICorsPolicyService)).LastOrDefault();
+            if (existingCors != null &&
+                existingCors.ImplementationType == typeof(DefaultCorsPolicyService) &&
+                existingCors.Lifetime == ServiceLifetime.Transient)
+            {
+                builder.Services.AddTransient<ICorsPolicyService, CustomCorsPolicyService>();
+            }
+
+            return builder;
+        }
+    }
+
+    public class CustomCorsPolicyService : ICorsPolicyService
+    {
+        public Task<bool> IsOriginAllowedAsync(string origin)
+        {
+            return Task.FromResult(origin.Equals("http://localhost:4200"));
+        }
+    }
+}
---
+++ src/Violetum.SPA/app/src/app/app.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core';
+import {OidcSecurityService} from 'angular-auth-oidc-client';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  constructor(
+    public oidcSecurityService: OidcSecurityService,
+    public http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.oidcSecurityService
+      .checkAuth()
+      .subscribe((auth) => console.log('is authenticated', auth));
+  }
+
+  login() {
+    this.oidcSecurityService.authorize();
+  }
+
+  callApi() {
+    const token = this.oidcSecurityService.getToken();
+
+    this.http.get("http://localhost:5001/api/v1/posts?OrderByDir=desc&SortBy=VoteCount&CurrentPage=1", {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + token,
+      }),
+      responseType: 'text',
+    })
+      .subscribe((data: any) => {
+        console.log(JSON.parse(data));
+      });
+  }
+}
