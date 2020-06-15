cb84d9a52c8f7a14921a9d56b9fe12621d686142
Tornike Goshadze <joffarex@gmail.com>
2020-06-15 10:00:53 +0400

    Implement redis with custom service and caching attribute

---
+++ src/Violetum.API/Cache/RedisCacheSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Violetum.API.Cache
+{
+    public class RedisCacheSettings
+    {
+        public bool Enabled { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
---
+++ src/Violetum.API/Filters/CachedAttribute.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Primitives;
+using Violetum.API.Cache;
+
+namespace Violetum.API.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class CachedAttribute : Attribute, IAsyncActionFilter
+    {
+        private readonly int _timeToLiveSeconds;
+
+        public CachedAttribute(int timeToLiveSeconds)
+        {
+            _timeToLiveSeconds = timeToLiveSeconds;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var cacheSettings = context.HttpContext.RequestServices.GetRequiredService<RedisCacheSettings>();
+
+            if (!cacheSettings.Enabled)
+            {
+                await next();
+                return;
+            }
+
+            var cacheService = context.HttpContext.RequestServices.GetRequiredService<IResponseCacheService>();
+
+            string cacheKey = GenerateCacheKeyFromRequest(context.HttpContext.Request);
+            string cachedResponse = await cacheService.GetCachedResponse(cacheKey);
+
+            if (!string.IsNullOrEmpty(cachedResponse))
+            {
+                var contentResult = new ContentResult
+                {
+                    Content = cachedResponse,
+                    ContentType = "application/json",
+                    StatusCode = (int) HttpStatusCode.OK,
+                };
+
+                context.Result = contentResult;
+                return;
+            }
+
+            ActionExecutedContext executedContext = await next();
+
+            if (executedContext.Result is OkObjectResult objectResult)
+            {
+                await cacheService.CacheResponse(cacheKey, objectResult.Value,
+                    TimeSpan.FromSeconds(_timeToLiveSeconds));
+            }
+        }
+
+        private static string GenerateCacheKeyFromRequest(HttpRequest request)
+        {
+            var keyBuilder = new StringBuilder();
+            keyBuilder.Append($"{request.Path}");
+
+            foreach ((string key, StringValues value) in request.Query.OrderBy(x => x.Key))
+            {
+                keyBuilder.Append($"|{key}-{value}");
+            }
+
+            return keyBuilder.ToString();
+        }
+    }
+}
