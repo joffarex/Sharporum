9981d39157ef1a0378b7bbb738c6e387dfbbeaf1
Tornike Goshadze <joffarex@gmail.com>
2020-07-05 14:35:27 +0400

    Small cleanup + bugfix

--- src/Violetum.ApplicationCore/Services/FollowerService.cs
+++ src/Violetum.ApplicationCore/Services/FollowerService.cs
@@ -29,7 +29,7 @@ namespace Violetum.ApplicationCore.Services
             Guard.Against.NullItem(user, nameof(user));
 
             IEnumerable<FollowerViewModel> userFollowers =
-                await _userRepository.ListUserFollowingAsync<FollowerViewModel>(userId,
+                await _userRepository.ListUserFollowersAsync<FollowerViewModel>(userId,
                     UserHelpers.GetFollowerMapperConfiguration());
 
             return new UserFollowersViewModel
